eb8af84853dee2d1fed5bf44b07e6b7853e5d554
Sven Axelsson <sven.axelsson@wakakuu.com>
2016-05-20 16:26:13 +0200

    Image renderer needs file_exists

--- src/app/code/community/FireGento/GridControl/Block/Adminhtml/Template/Grid/Renderer/Image.php
+++ src/app/code/community/FireGento/GridControl/Block/Adminhtml/Template/Grid/Renderer/Image.php
@@ -4,7 +4,7 @@ class FireGento_GridControl_Block_Adminhtml_Template_Grid_Renderer_Image extends
     public function render(Varien_Object $row)
     {
         $width = 75;
-        if ($row->hasData('image')) {
+        if ($row->hasData('image')  && file_exists('media/catalog/product'. $row->getImage()) ) {
             $image = Mage::helper('catalog/image')->init($row, 'image')->resize($width);
             return '<img src="'. $image .'" width="' . $width .'px" style="margin: 0 auto; display: block;"/>';
         }
